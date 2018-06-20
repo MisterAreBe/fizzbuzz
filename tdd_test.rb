e0212d265a0fb301db93e55b38df128a9c1d4b74
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 13:50:14 -0400

    added extra tests to further check

--- tdd_test.rb
+++ tdd_test.rb
@@ -21,14 +21,20 @@ class TestTddFizz < Minitest::Test
     
     def test_third_element_returns_mined
         assert_equal("mined", fizzbuzz()[2])
+        assert_equal("mined", fizzbuzz()[11])
+        assert_equal("mined", fizzbuzz()[62])
     end
 
     def test_fifth_element_returns_minds
         assert_equal("minds", fizzbuzz()[4])
+        assert_equal("minds", fizzbuzz()[19])
+        assert_equal("minds", fizzbuzz()[49])
     end
 
     def test_15th_element_returns_mined_minds
         assert_equal("mined minds", fizzbuzz()[14])
+        assert_equal("mined minds", fizzbuzz()[29])
+        assert_equal("mined minds", fizzbuzz()[59])
     end
 
 end
